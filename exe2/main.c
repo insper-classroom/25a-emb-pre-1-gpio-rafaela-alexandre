60fe57f4231fb09828cfc3950cd83729cc849c79
rafaela-alexandre <rafaelaoa@al.insper.edu.br>
2025-02-11 21:38:42 -0300

    ex2

--- exe2/main.c
+++ exe2/main.c
@@ -2,19 +2,35 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-const int BTN_PIN = 26;
+const int BTN_PIN_G= 26;
+const int BTN_PIN_B= 7;
+
 
 int main() {
   stdio_init_all();
 
-  gpio_init(BTN_PIN);
-  gpio_set_dir(BTN_PIN, GPIO_IN);
-  gpio_pull_up(BTN_PIN);
+  gpio_init(BTN_PIN_G);
+  gpio_init(BTN_PIN_B);
+
+  gpio_set_dir(BTN_PIN_G, GPIO_IN);
+  gpio_set_dir(BTN_PIN_B, GPIO_IN);
+
+  gpio_pull_up(BTN_PIN_G);
+  gpio_pull_up(BTN_PIN_B);
+
 
   while (true) {
-    if (!gpio_get(BTN_PIN)) {
-      printf("Botao 1\n");
-      while (!gpio_get(BTN_PIN)) {
+
+    if (!gpio_get(BTN_PIN_G)) {
+      printf("Botao Green\n");
+      while (!gpio_get(BTN_PIN_G)) {
+        sleep_ms(10);
+      };
+    }
+    if (!gpio_get(BTN_PIN_B)) {
+      printf("Botao Azul\n");
+      while (!gpio_get(BTN_PIN_B)) {
+        sleep_ms(10);
       };
     }
   }
